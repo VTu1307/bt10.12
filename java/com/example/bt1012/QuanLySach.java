d3236b3ef6f749afbccc071b08986d5fc2c78e3b
VTu1307 <tutv.24ite@vku.udn.vn>
2024-12-10 21:22:04 +0700

    Update QuanLySach.java

--- java/com/example/bt1012/QuanLySach.java
+++ java/com/example/bt1012/QuanLySach.java
@@ -1,9 +1,9 @@
 package com.example.bt1012;
 
 public interface QuanLySach {
-    void themSachMoi(Book book);  // Thêm sách mới
-    void xoaSach(int bookId);     // Xóa sách
-    Book timSach(String keyword); // Tìm kiếm sách theo tiêu đề hoặc tác giả
-    void hienThiDanhSach();       // Hiển thị danh sách sách
+    void themSachMoi(Book book);  
+    void xoaSach(int bookId);    
+    Book timSach(String keyword); 
+    void hienThiDanhSach();    
 }
 
